5433bc003ad4b76c4034172ddca97054d6d0fbe2
Oscar Urenda Moix <124069546+oscarurenda2001@users.noreply.github.com>
2026-01-31 23:37:07 +0100

    Add Tracer + item picking logic
    Add Tracer + item picking logic

--- Enmascarando-crimen-game/camera_3d.gd
+++ Enmascarando-crimen-game/camera_3d.gd
@@ -1,15 +1,27 @@
 extends Camera3D
 
 @export var follow_target: Node3D
-@export var rayo: RayCast3D
-# Called when the node enters the scene tree for the first time.
+@export var ray_length: float = 100.0  # distancia del raycast
+var items = ['StaticCuerpo','StaticSangre','StaticCuchillo','StaticPisada']
+
+func check_if_object():
+	var space_state = get_world_3d().direct_space_state
+	var ray_params = PhysicsRayQueryParameters3D.new()
+	ray_params.from = global_position
+	ray_params.to = global_position + -global_transform.basis.z * ray_length
+	ray_params.collision_mask = 1  
+	var result = space_state.intersect_ray(ray_params)
+	if result:
+		var collider = result.collider
+		if(result.collider.name in items):
+			print(result)
+			print(collider.get_parent().get_parent())
+			var obj = collider.get_parent().get_parent()
+			obj.interact(obj)
+
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+
 	global_position = follow_target.global_position + Vector3(0, 30, 0)
-	if Input.is_action_just_pressed("click"):
-		if rayo.is_colliding():
-			var obj = rayo.get_collider()
-			obj.clicked()
