ec1086b6877f94bb5b22b0d26957d8650026e02d
Alicia-Ortega <alicia.ocf.22@gmail.com>
2026-01-31 09:23:23 +0100

    is walking bitches

--- Enmascarando-crimen-game/camera_3d.gd
+++ Enmascarando-crimen-game/camera_3d.gd
@@ -1,7 +1,6 @@
 extends Camera3D
 
 @export var follow_target: Node3D
-@export var look_target: Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,4 +10,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	global_position = follow_target.global_position
-	look_at(look_target)
--- Enmascarando-crimen-game/character_body_3d.gd
+++ Enmascarando-crimen-game/character_body_3d.gd
@@ -1,28 +1,40 @@
 extends CharacterBody3D
 
+@export var camera: Node3D
 
-const SPEED = 5.0
+var count := 0
+var yaw := 0.0
+var pitch := 0.0
+const PITCH_MIN := deg_to_rad(-60)
+const PITCH_MAX := deg_to_rad(60)
+const SENS := 0.008
+const SPEED = 15.0
 const JUMP_VELOCITY = 4.5
 
-
+func _input(event):
+	if event is InputEventMouseMotion:
+		yaw -= event.relative.x * SENS
+		pitch -= event.relative.y * SENS
+		pitch = clamp(pitch, PITCH_MIN, PITCH_MAX)
+		
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	camera.rotation = Vector3(pitch, yaw, 0.0)
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+
+	if Input.is_action_pressed("palante"):
+		velocity.x = -sin(yaw) * SPEED
+		velocity.z = -cos(yaw) * SPEED
+		if count % 2 and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	count += 1
 	move_and_slide()
