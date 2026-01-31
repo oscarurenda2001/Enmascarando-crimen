7660261354fc50c18866b58630439971c318010d
SoyFong <xjara.bellmunt@gmail.com>
2026-02-01 00:28:08 +0100

    sonidos en mainCharacter

--- Enmascarando-crimen-game/camera_3d.gd
+++ Enmascarando-crimen-game/camera_3d.gd
@@ -2,6 +2,7 @@ extends Camera3D
 
 @export var follow_target: Node3D
 @export var rayo: RayCast3D
+@onready var grab = $"../grab"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -11,5 +12,6 @@ func _process(delta: float) -> void:
 	global_position = follow_target.global_position + Vector3(0, 30, 0)
 	if Input.is_action_just_pressed("click"):
 		if rayo.is_colliding():
+			grab.play()
 			var obj = rayo.get_collider()
 			obj.clicked()
--- Enmascarando-crimen-game/character_body_3d.gd
+++ Enmascarando-crimen-game/character_body_3d.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 
 @export var camera: Node3D
 @export var arms : Brazos
+@onready var footsteps = $footsteps
 
 var count := 0
 var yaw := 0.0
@@ -30,23 +31,30 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-
 	if Input.is_action_pressed("palante"):
+		if !footsteps.playing:
+			footsteps.play()
 		velocity.x = -sin(yaw) * SPEED
 		velocity.z = -cos(yaw) * SPEED
 		if count % 2 and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 	elif Input.is_action_pressed("patras"):
+		if !footsteps.playing:
+			footsteps.play()
 		velocity.x = sin(yaw) * SPEED
 		velocity.z = cos(yaw) * SPEED
 		if count % 2 and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 	elif Input.is_action_pressed("pedroSanche"):
+		if !footsteps.playing:
+			footsteps.play()
 		velocity.x = -cos(yaw) * SPEED
 		velocity.z = sin(yaw) * SPEED
 		if count % 2 and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 	elif Input.is_action_pressed("Vox"):
+		if !footsteps.playing:
+			footsteps.play()
 		velocity.x = cos(yaw) * SPEED
 		velocity.z = -sin(yaw) * SPEED
 		if count % 2 and is_on_floor():
@@ -54,6 +62,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		footsteps.stop()
 	count += 1
 	move_and_slide()
